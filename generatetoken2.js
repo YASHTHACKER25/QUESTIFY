24fcfb5cd590aaf7673119707c4022e6d03a45c7
YASHTHACKER25 <yashthacker5340@gmail.com>
2025-11-18 19:36:55 +0530

    Delete generatetoken2.js

--- generatetoken2.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-import jwt from "jsonwebtoken"
-export function generateToken(userid){
-    return jwt.sign({id:userid},process.env.ACSESS_TOKEN,{expiresIn:"1h"})
-}
-export function generateRefreshToken(userid){
-    return jwt.sign({id:userid},process.env.REFRESH_TOKEN,{expiresIn:"7d"})
-}
